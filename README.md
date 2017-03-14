5f26c099717530d7b9144e6e0dcbe715b6ed3105
Andrew Schmelyun <schmelyun@gmail.com>
2017-03-14 15:30:52 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ip-visitor-count
-Return visits by IP address while looking for a specific string in access logs
+A simple tool that lets you search through a standard unix access log to determine visitor count by IP address of a particular path or string.
+
+### Why?
+
+I built this trying to determine the most frequent IP addresses hitting the login page of my application, and this seemed like the easiest solution.
+
+### Installation
+
+You can just clone this repo and use the included script as-is with `php ip_visitor_count.php` or for easier access you can do:
+
+* `chmod +x ip_visitor_count.php`
+* `sudo mv ip_visitor_count.php /usr/local/bin/ipvc`
+
+The script can now be accessed with just the `ipvc` command. :tada:
+
+
+### Usage
+
+The syntax of the script is `ipvc access.log 'string to search'`. So for example, I want to see what the most frequent IP addresses are that are accessing my WordPress login page, I would do `ipvc access.log 'GET /wp-login.php'`. The script runs, and returns an array of ip address => hits sorted in descending order. :fire: :fire: :fire:
---
+++ ip_visitor_count.php
@@ -0,0 +1,44 @@
+#!/usr/bin/php
+<?php
+
+// foreach line in the log
+// only continue with ones that contain the string we're looking for
+// determine the ip of that access
+// if an index exists in the array for that ip, add to the visit count
+// else create an index for that ip, add a visit count of one
+// display the array of ip addresses with corresponding visits
+
+// ex: ipvc access.log '/wp-login.php'
+
+if( empty($argv[1]) || empty($argv[2]) ) {
+    echo 'Error: Proper usage is ipvc access.log login.php' . PHP_EOL;
+    exit();
+}
+
+$filename = $argv[1];
+$filepath = getcwd() . '/' . $filename;
+$search = $argv[2];
+$visits = array();
+
+$handle = fopen( $filepath, "r" );
+if ($handle) {
+    while ( ($line = fgets($handle)) !== false ) {
+        if ( strpos($line, $search) !== false ) {
+            $ip = explode(" - - ", $line)[0];
+            if( !empty($visits[$ip]) ) {
+                $visits[$ip]++;
+            } else {
+                $visits[$ip] = 1;
+            }
+        }
+    }
+
+    fclose($handle);
+} else {
+    echo 'Error: Could not process the file given' . PHP_EOL;
+}
+
+arsort($visits);
+echo print_r($visits, true);
+
+?>
